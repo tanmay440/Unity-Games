a093f01040b3f4e7acf6c4e1542fb43487fbf474
tanmay440 <62497552+tanmay440@users.noreply.github.com>
2021-05-14 09:24:39 +0530

    V 2.0 Tower Defence Game

---
+++ Tower defence game v2.0/tdg/Assets/enemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointindex = 0;
+    void Start()
+    {
+        target = waypoints.points[0];
+    }
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    void GetNextWaypoint(){
+        if (wavepointindex >= waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        wavepointindex++ ;
+        target = waypoints.points[wavepointindex];
+    }
+}
